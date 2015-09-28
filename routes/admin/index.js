ae2741bf975b488e936d8a9e2623e7d083d2338a
saucebwz <saucebwz@yahoo.com>
2015-09-28 23:51:49 +0300

    Fixed permission bugs

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,11 +3,12 @@ var User = require('../../models/users').User;
 var async = require('async');
 var router = express.Router();
 var Conference = require('../../models/room').Conference;
-
+var _ = require('underscore');
 router.post('/inviteuser', function(req, res, next){
     var slug = req.body.roomSlug;
     var userToInvite= req.body.inviteUsername;
     var username = req.user.username;
+    console.log("test");
     Conference.findOne({slug: slug}, function(err, conference){
         if(err) return next(new Error("Database error!"));
         if(!conference) return next(new Error(404, "No conference found!"));
@@ -40,15 +41,17 @@ router.post('/sendpermissions', function(req, res, next){
         if (err) return next(new Error("Database error!"));
         if (!conference) return next(new Error(404, "No conference found!"));
         if (!conference.isOwner(username)) return next(new Error(403, "No permissions!"));
-        conference.permissions.member = memberPermissions;
-        conference.permissions.user = userPermissions;
+        var old_perms = _.clone(conference.permissions);
+        old_perms.user = userPermissions;
+        old_perms.member = memberPermissions;
+        conference.permissions = old_perms;
+
         conference.save(function(err){
             if(err){
                 return next(err);
             }
+            res.json({success: "Permissions saved!"})
         });
-
-        res.json({success: "Permissions saved!"})
     });
 });
 module.exports = router;
