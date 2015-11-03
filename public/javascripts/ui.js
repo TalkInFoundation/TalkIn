1bcf92c79b3e364e99641dba91e9243c11bc9871
Keyten <ikeyten@gmail.com>
2015-11-03 22:13:32 +0300

    Added widgets

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -54,3 +54,139 @@ $(function(){
 	});
 
 });
+
+var Widget = (function($, undefined){
+
+	var $document = $(document);
+
+	function Widget(name, params){
+		if( !(name in Widget.widgets) ){
+			throw "Undefined widget: " + name;
+		}
+
+		Object.keys(Widget.widgets[name]).forEach(function(param){
+			this[param] = Widget.widgets[name][param];
+		}.bind(this));
+
+		this.makeHTML(params);
+		this.make(params);
+	}
+
+	Widget.prototype = {
+
+		makeHTML: function(params){
+
+			this.$panel = $('<div></div>', {
+				class: 'panel',
+				css: {
+					width: this.width,
+					height: this.height,
+					position: 'absolute',
+					top: this.top,
+					left: this.left
+				}
+			}).appendTo('body');
+
+			this.$title = $('<div class="panel_title"></div>').appendTo(this.$panel);
+			this.$buttons = $('<div class="panel_title_buttons"></div>').appendTo(this.$title);
+			this.$title.append(this.title)
+			this.$content = $('<div class="panel_content"></div>').appendTo(this.$panel);
+
+			this.makeButtons();
+			this.makeInteraction();
+
+		},
+
+		makeButtons: function(){
+
+			if(this.collapsible){
+
+				this.$collapseButton = this.makeButton('xe80f', function(){
+					if(this.collapsed){
+						this.$collapseButton.html('&#xe80f;');
+						this.$content.slideDown();
+						this.collapsed = false;
+					}
+					else {
+						this.$collapseButton.html('&#xe810;');
+						this.$content.slideUp();
+						this.collapsed = true;
+					}
+				}.bind(this));
+
+			}
+
+		},
+
+		makeButton: function(symbol, listener, className){
+
+			var $button = $('<i class="icon">&#' + symbol + ';</i>', {
+				class: className
+			}).appendTo(this.$buttons);
+
+			$button.click(listener);
+
+			return $button;
+
+		},
+
+		makeInteraction: function(){
+
+			if(this.draggable){
+
+				var x, y;
+				this.$title.mousedown(function(e){
+					var bounds = this.$panel[0].getBoundingClientRect();
+					x = e.clientX - bounds.left;
+					y = e.clientY - bounds.top;
+
+					document.body.style.MozUserSelect = 'none';
+					document.body.style.WebkitUserSelect = 'none';
+				}.bind(this));
+
+				$document.mouseup(function(){
+					x = null;
+					y = null;
+
+					document.body.style.MozUserSelect = '';
+					document.body.style.WebkitUserSelect = '';
+				}).mousemove(function(e){
+					if(!x || !y)
+						return;
+					this.$panel.css({
+						top: e.clientY - y,
+						left: e.clientX - x
+					});
+				}.bind(this));
+			}
+
+		},
+
+		collapsible: true,
+		collapsed: false,
+		draggable: true,
+		resizable: true
+
+	};
+	Widget.widgets = {};
+
+	return Widget;
+
+})(jQuery);
+
+/*
+Examples:
+
+Widget.widgets['Test'] = {
+	title: 'Test',
+	top: 50,
+	left: 0,
+	width: 200,
+	//height: 200,
+	make: function(params){
+		this.$content.text(params.msg);
+	}
+};
+
+var wid = new Widget('Test', { msg: 'test' });
+ */
